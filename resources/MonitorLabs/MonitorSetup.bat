5fb7ecca1caaad0f349830bb010b84394d818692
edward616 <Haibing.Zhou@software.dell.com>
2015-05-28 18:29:53 +0800

    clean resource

--- resources/MonitorLabs/MonitorSetup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set currentPath=%~dp0
-set "psStript=powershell -ExecutionPolicy ByPass -File %currentPath%GetRemoteDesktopInfo.ps1"
-echo %psStript%
-schtasks /create /sc minute /mo 1 /tn "MonitorSetup" /tr "%psStript%"
-pause
